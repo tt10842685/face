ebe640d7daa130f66753eae9c1bdf1bcc3ed6db3
tt10842685 <10842685@qq.com>
2019-03-29 15:56:17 +0800

    first commit

---
+++ ArcfaceDemo/app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "C5VMiRDvzpAqtKwbeLWRF4gL8gdPnhwHkJQ2tc3Qi8mM";
+    public static final String SDK_KEY = "8eAkE3bchAVTkpsEJx6yb6cYhtXqp8vwWwE3UU6bHNUH";
+}
